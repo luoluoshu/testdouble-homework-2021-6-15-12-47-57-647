35e6ce6c1d2ec04665003e5479a6b18e300219c4
Thomas Wang <wglcoco@gmail.com>
2021-06-16 23:18:24 +0800

    Update PrinterTest.java
    update PrinterTest

--- src/test/java/com/tw/banking/PrinterTest.java
+++ src/test/java/com/tw/banking/PrinterTest.java
@@ -1,24 +1,45 @@
 package com.tw.banking;
 
 import static com.tw.banking.Printer.STATEMENT_HEADER;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class PrinterTest {
-
     @Test
-    void should_invoke_console_printLine_when_call_print_given_anyTransactions() {
+    void should_print_statement_header_when_call_print_given_anyTransactions() {
         Console spyConsole = mock(Console.class);
         Printer printer = new Printer(spyConsole);
 
         printer.print(anyList());
 
         verify(spyConsole, times(1)).printLine(STATEMENT_HEADER);
+        verify(spyConsole, times(1)).printLine(Printer.STATEMENT_HEADER);
     }
 
+    @Test
+    void should_not_change_given_transactions_when_call_print() {
+        Console dummyConsole = mock(Console.class);
+        Printer printer = new Printer(dummyConsole);
+        Transaction transaction1 = new Transaction("23/02/2019", 1);
+        Transaction transaction2 = new Transaction("23/02/2018", 1);
+        List<Transaction> givenTransactions = Arrays.asList(transaction1, transaction2);
+
+        printer.print(givenTransactions);
+
+        List<Transaction> expectedTransactions = Arrays.asList(transaction1, transaction2);
+        assertTransactionsEquals(expectedTransactions, givenTransactions);
+    }
+
+    private void assertTransactionsEquals(List<Transaction> expectedTransactions, List<Transaction> givenTransactions) {
+        for (int i = 0; i < expectedTransactions.size(); i++) {
+            assertEquals(expectedTransactions.get(i), givenTransactions.get(i));
+        }
+    }
 }
